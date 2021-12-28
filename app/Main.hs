b462c9c39f58bf376f22986da3a47f0eb708a2e5
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2021-12-28 23:49:52 +0100

    First bridge
    - Verlet integrator
    - warmup phase (apply the computation a few times without gravity until
      stability is found)

--- app/Main.hs
+++ app/Main.hs
@@ -8,48 +8,81 @@ import qualified Data.Map.Strict as Map
 import Debug.Trace (traceShow)
 import Graphics.Gloss
 import Graphics.Gloss.Data.ViewPort
-import Graphics.Gloss.Interface.Pure.Game (Event)
+import Graphics.Gloss.Interface.Pure.Game (Event (EventKey), Key (SpecialKey), SpecialKey (KeySpace))
 import Linear
 import GHC.Stack (HasCallStack)
 import Data.Maybe (fromMaybe)
+import GHC.Float
 
-data NodeMass = Fixed | Mass Float
+data NodeMass = Fixed | Mass Double
   deriving (Show, Eq)
 
 data Node = Node
-  { position :: V2 Float,
-    mass :: NodeMass,
-    velocity :: V2 Float
+  { position :: V2 Double,
+    position' :: V2 Double,
+    mass :: NodeMass
   }
   deriving (Show)
 
-fixedNode p m = Node p m (V2 0 0)
+fixedNode p m = Node p p m
 
 data Edge = Edge Int Int
   deriving (Show)
 
 data World = World
   { nodes :: Map Int Node,
-    edges :: [Edge]
+    edges :: [Edge],
+    g :: Double
   }
   deriving (Show)
 
+h = sqrt 125
+
 initWorld =
   World
-    { nodes =
+    { 
+      g = 0,
+      nodes =
         Map.fromList
-          [ (0, fixedNode (V2 0 0) Fixed),
-            (1, fixedNode (V2 10 0) (Mass 1)),
-            (2, fixedNode (V2 20 0) (Mass 1))
+          [ (0, fixedNode (V2 (-20) 0) Fixed),
+            (1, fixedNode (V2 (-10) 0) (Mass 1)),
+            (2, fixedNode (V2 0 0) (Mass 1)),
+            (3, fixedNode (V2 10 0) (Mass 1)),
+            (4, fixedNode (V2 20 0) Fixed),
+
+            (5, fixedNode (V2 (-15) (-h)) (Mass 1)),
+            (6, fixedNode (V2 (-5) (-h)) (Mass 1)),
+            (7, fixedNode (V2 5 (-h)) (Mass 1)),
+            (8, fixedNode (V2 15 (-h)) (Mass 1))
           ],
       edges =
-        [ Edge 0 1,
-          Edge 1 2 
+        [ 
+          Edge 0 1,
+          Edge 1 2,
+          Edge 2 3,
+          Edge 3 4,
+
+          -- ZigZag shape
+          Edge 0 5,
+          Edge 1 5,
+          Edge 1 6,
+          Edge 2 6,
+          Edge 2 7,
+          Edge 3 7,
+          Edge 3 8,
+          Edge 4 8,
+
+          -- Top
+          Edge 5 6,
+          Edge 6 7,
+          Edge 7 8
         ]
     }
 
 main :: IO ()
-main = play (InWindow "LambdaBridge" (800, 600) (0, 0)) white 20 initWorld draw event step
+main = play (InWindow "LambdaBridge" (800, 600) (0, 0)) white 20 (warmUp initWorld) draw event (step' 1000)
+
+warmUp w = (applyN 200 (step' 100 (1 / 20)) w) { g = 9.8 }
 
 draw :: World -> Picture
 draw w =
@@ -58,15 +91,20 @@ draw w =
       <> foldMap (drawEdge (nodes w)) (edges w)
 
 event :: Event -> World -> World
+event (EventKey (SpecialKey KeySpace) _ _ _) w = w { edges = drop 1 (edges w) }
 event e w = traceShow e w
-
-g = 9.8
 
 z = V2 0 (-1)
 
-k = 10
+k = 1000
+kDamp = 1
 
-step :: Float -> World -> World
+applyN 0 _ v = v
+applyN n f v = applyN (n - 1) f (f v)
+
+step' n (float2Double -> dt) = applyN n (step (dt / n))
+
+step :: Double -> World -> World
 step dt w = w {nodes = nodes'}
   where
     forces_on_nodes = Map.fromListWith (+) $ do
@@ -81,24 +119,27 @@ step dt w = w {nodes = nodes'}
       [(nodeI, f), (nodeJ, - f)]
 
     nodes' = Map.fromList $ do
-      (nodeId, Node p mass v) <- Map.toList (nodes w)
+      (nodeId, Node p pPrev m) <- Map.toList (nodes w)
 
       -- Add springs
-      let accel = case mass of
+      let accel = case m of
             Fixed -> 0
             Mass m ->
-              let forces = m * g *^ z + (fromMaybe (V2 0 0) (Map.lookup nodeId forces_on_nodes))
-               in forces ^/ m
-      let v' = v + accel ^* dt
-      let p' = p + v' ^* dt
+              let spring = (fromMaybe (V2 0 0) (Map.lookup nodeId forces_on_nodes))
+                  damping = -kDamp * v
+                  gravity = m * (g w) *^ z
+                  v = (p - pPrev) ^/ dt
+               in (spring + damping + gravity) ^/ m
+      let pPrev' = p
+          p' = 2 * p - pPrev + accel ^* (dt * dt)
 
-      pure (nodeId, Node p' mass v')
+      pure (nodeId, Node p' pPrev' m)
 
-drawNode (Node (V2 dx dy) mass velocity) = Translate dx dy $ Color (if mass == Fixed then red else black) $ Circle 1
+drawNode (Node (V2 dx dy) _ mass) = Translate (double2Float dx) (double2Float dy) $ Color (if mass == Fixed then red else black) $ Circle 1
 
 drawEdge nodes (Edge nodeA nodeB) = line [readPoint nodeA, readPoint nodeB]
   where
-    readPoint (position . (nodes !?) -> (V2 x y)) = (x, y)
+    readPoint (position . (nodes !?) -> (V2 x y)) = (double2Float x, double2Float y)
 
 viewport = viewPortInit {viewPortScale = 10}
 
